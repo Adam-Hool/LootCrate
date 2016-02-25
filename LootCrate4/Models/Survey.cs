bcea2d9fde11e887c3ffa18cd5012b16ce36aa0d
Adam <adamhool414@gmail.com>
2016-02-25 16:37:04 -0600

    surveyMade

--- LootCrate4/Models/Survey.cs
+++ LootCrate4/Models/Survey.cs
@@ -9,6 +9,7 @@ namespace LootCrate4.Models
     {
         public int ID { get; set; }
         public string question { get; set; }
+        public string questionAnswer { get; set; }
         public int questionScore { get; set; }
         public int totalScore { get; set; }
     }
